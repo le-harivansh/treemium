21a93809a4bcc28478dcb1d2fe9b46dd5bb8ed8c
Harivansh <hello@harivan.sh>
2023-01-14 16:42:55 +0400

    feat: setup laravel livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,9 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Livewire -->
+        <livewire:styles/>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -31,5 +34,7 @@
                 {{ $slot }}
             </main>
         </div>
+
+    <livewire:scripts/>
     </body>
 </html>
