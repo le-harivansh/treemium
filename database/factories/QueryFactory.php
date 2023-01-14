8ea5c0eced76eb87763c79f61f48ac0b6c8bc0a5
Harivansh <hello@harivan.sh>
2023-01-14 16:56:20 +0400

    feat: setup Query model

---
+++ database/factories/QueryFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Query;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Query>
+ */
+class QueryFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->email(),
+            'tel' => substr(str_shuffle("0123456789"), 0, rand(7, 14)),
+            'message' => fake()->text(),
+        ];
+    }
+}
