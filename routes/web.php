49ca5d828921c550a0c8a5ed681aa5e20070eb94
Harivansh <hello@harivan.sh>
2023-01-14 16:20:00 +0400

    refactor: remove unused controllers, views, & routes

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,41 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SendClientQueryController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
 
+// client
 Route::view('', 'client.index')->name('landing');
-
 Route::post('send-query', SendClientQueryController::class)->name('send-query');
-
 Route::view('query-sent', 'client.query-sent')->name('query-sent');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// admin
+Route::prefix('admin')->group(function () {
+    Route::middleware('guest')->group(function () {
+        Route::get('login', [AuthenticatedSessionController::class, 'create'])
+            ->name('login');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        Route::post('login', [AuthenticatedSessionController::class, 'store']);
+
+        Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
+            ->name('password.request');
+
+        Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+            ->name('password.email');
+
+        Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
+            ->name('password.reset');
+
+        Route::post('reset-password', [NewPasswordController::class, 'store'])
+            ->name('password.store');
+    });
+
+    Route::middleware('auth')->group(function () {
+        Route::view('dashboard', 'dashboard')->name('dashboard');
+
+        Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+            ->name('logout');
+    });
 });
-
-require __DIR__.'/auth.php';
