f7b4eb0c373cff158cbc55d16de0843d4625a1f3
Harivansh <hello@harivan.sh>
2023-01-14 16:19:19 +0400

    feat: add admin-creation command

---
+++ app/Console/Commands/CreateAdministrator.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class CreateAdministrator extends Command
+{
+    protected $signature = 'admin:new';
+
+    protected $description = 'Create a new administrator.';
+
+    public function handle(): int
+    {
+        $this->info('You are creating a new administrator. Enter the required information.');
+
+        $name = $this->ask('Name');
+        $email = $this->ask('Email');
+        $password = $this->secret('Password');
+
+        $newAdministrator = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+
+        $this->info("A new administrator has been created [name: {$newAdministrator->name}, email: {$newAdministrator->email}].");
+
+        return Command::SUCCESS;
+    }
+}
